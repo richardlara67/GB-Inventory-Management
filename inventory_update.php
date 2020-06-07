37599601d53f5ab6643465c355657d26779af155
Chase Musinski <38532186+cski96@users.noreply.github.com>
2020-06-07 13:07:21 -0500

    Add files via upload

---
+++ inventory_update.php
@@ -0,0 +1,17 @@
+
+<?php
+	include_once 'dbh.php';
+
+	$eid = $_POST['employeeID'];
+	$ename = $_POST['employeeName'];
+	$dou = $_POST['date'];
+	$ec = $_POST['equipmentCode']
+	$in = $_POST['actionType'];
+	$out = $_POST['actionType'];
+
+
+	$sql = "INSERT INTO inventory (employee_id, employee_name, date_of_use, check_out, check_in) VALUES ('$eid', $ename', '$dou', '$ec', '$in', '$out');";
+
+	mysqli_query($conn, $sql);
+
+	header("Location: ../index.php?inventory_update=success");
