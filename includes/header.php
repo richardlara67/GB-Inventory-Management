2a47be1177741cf2b54b8d8548f408b8fd8dfb3c
Richard Lara <richardlara67@gmail.com>
2020-06-25 14:44:12 -0500

    Dynamic Navigation
    Features
    * Created array for main menu
    
    Fixes:
    Active link is now highlighted

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,7 @@
  *
  *
 **/
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,26 +17,4 @@
 	<title><?php echo($title); ?></title>
 </head>
 <body>
-	<div class="topheader">
-		<nav class="navbar navbar-expand-lg navbar-dark primary-color">
-		  <div class="container">
-			<a class="navbar-brand" href="index.php">GB Inventory Management</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
-			  <span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="main-menu">
-			  <ul class="navbar-nav mr-auto">
-				<li class="nav-item active">
-				  <a class="nav-link" href="index.php">Use equipment <span class="sr-only">(current)</span></a>
-				</li>
-				<li class="nav-item">
-				  <a class="nav-link" href="inventory_log.php">View inventory</a>
-				</li>
-				<li class="nav-item">
-				  <a class="nav-link" href="update_log.php">Update inventory</a>
-				</li>  
-			  </ul>
-			</div>
-		   </div>
-		</nav>
-	</div>
+	<?php include('includes/navigation.php'); ?>
--- update_log.php
+++ update_log.php
@@ -2,28 +2,28 @@
 $title = 'Inventory Update';
 include('includes/header.php'); 
 ?>	
-	<div class="container py-5">
-		<form>
-				<div class="form-group row">
-					<label for="inputEmail3" class="col-sm-2 col-form-label">Equipment Code</label>
-					<div class="col-sm-10">
-						<input type="email" class="form-control" id="inputEmail3" placeholder="Equipment Code">
-					</div>
-				</div>
-				<div class="form-group row">
-					<label for="inputPassword3" class="col-sm-2 col-form-label">Quantity</label>
-					<div class="col-sm-10">
-						<input type="password" class="form-control" id="inputPassword3" placeholder="Quantity">
-					</div>
-				</div>
-				
-			<div class="form-group row">
-				<div class="col-sm-10">
-					<button type="submit" class="btn btn-primary">Update</button>
-				</div>
+<div class="container py-5">
+	<form>
+		<div class="form-group row">
+			<label for="inputEmail3" class="col-sm-2 col-form-label">Equipment Code</label>
+			<div class="col-sm-10">
+				<input type="email" class="form-control" id="inputEmail3" placeholder="Equipment Code">
 			</div>
-		</form>
-	</div>
+		</div>
+		<div class="form-group row">
+			<label for="inputPassword3" class="col-sm-2 col-form-label">Quantity</label>
+			<div class="col-sm-10">
+				<input type="password" class="form-control" id="inputPassword3" placeholder="Quantity">
+			</div>
+		</div>
+
+		<div class="form-group row">
+			<div class="col-sm-10">
+				<button type="submit" class="btn btn-primary">Update</button>
+			</div>
+		</div>
+	</form>
+</div>
 
 
 <?php
